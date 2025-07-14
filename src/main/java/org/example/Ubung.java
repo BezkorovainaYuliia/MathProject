690f266f5720cf9ff7787c4864a655f9d6121bd3
yuliiabezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-07-14 09:43:23 +0200

    add Ubung.java edit Ubung.java main-Methode

---
+++ src/main/java/org/example/Ubung.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.math.BigDecimal;
+
+public class Ubung {
+    public static void main(String[] args) {
+        BigDecimal amount = new BigDecimal("100");
+        amount = amount.divide(new BigDecimal("100"));
+        System.out.println(amount);
+    }
+}
