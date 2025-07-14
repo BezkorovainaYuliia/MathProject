83f268852d1377d26683b16c9cfc2c1f972412fc
yuliiabezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-07-14 09:49:26 +0200

    replace divide -> add edit Ubung.java main-Methode

--- src/main/java/org/example/Ubung.java
+++ src/main/java/org/example/Ubung.java
@@ -6,9 +6,11 @@ public class Ubung {
     public static void main(String[] args) {
         BigDecimal amount = new BigDecimal("100.00");
         System.out.println(amount);
-        amount = amount.divide(new BigDecimal("100"));
+        amount = amount.add(new BigDecimal("100"));
         System.out.println(amount);
         System.out.println("Branch2");
         System.out.println("Branch22");
+        System.out.println("Branch222");
+        System.out.println("Branch22222");
     }
 }
