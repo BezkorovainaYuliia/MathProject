c733bb0758645fef75dc71c190f86e5750f238a6
yuliiabezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-07-14 09:36:35 +0200

    Second Version

---
+++ src/main/java/org/example/Ubung.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.math.BigDecimal;
+
+public class Ubung {
+    public static void main(String[] args) {
+        BigDecimal amount = new BigDecimal("100.00");
+        System.out.println(amount);
+        amount = amount.divide(new BigDecimal("100"));
+        System.out.println(amount);
+        System.out.println("Branch2");
+        System.out.println("Branch22");
+    }
+}
